29248787de05b019eacfc3a47584ccd1ec2eaa95
ltgranada <luisenrico.granada.cics@ust.edu.ph>
2025-05-19 23:48:07 +0800

    luis

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,7 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-
+import 'package:fl2_arangali/main.dart';
 import 'package:fl2_arangali/signup.dart';
 
 void main() {
